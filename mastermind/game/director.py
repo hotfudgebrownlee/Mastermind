14467f265b739af5edce9e2c94213c0894295999
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-02-10 10:45:57 -0700

    added director class

---
+++ mastermind/game/director.py
@@ -0,0 +1,16 @@
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to control the sequence of play.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+    """
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        pass
