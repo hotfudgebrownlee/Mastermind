054b77e2931c4109ee29922b528debf6f125ff40
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-02-11 12:01:41 -0700

    console class

--- mastermind/game/console.py
+++ mastermind/game/console.py
@@ -1,12 +1,42 @@
 class Console:
-    """
+    """Code template for a computer console. This class
+    is to get a text/numerical inputs and to display text outputs.
+
     Stereotype:
         Service Provider, Interfacer
+
+    Attributes:
+        prompt(string): The prompt to display.
     """
-    def __init__(self):
-        """The class constructor.
+    def read(self, prompt):
+        """Gets text input from the user.
         
         Args:
             self (Console): an instance of Console.
+            prompt(string): The prompt to be displayed.
+
+        Returns:
+            string: the user's input as text.
         """
-        pass
+        return input(prompt)
+
+    def read_number(self, prompt):
+        """Gets text input from the user.
+        
+        Args:
+            self (Console): an instance of Console.
+            prompt(string): The prompt to be displayed.
+
+        Returns:
+            integer: the user's input as a number.
+        """
+        return int(input(prompt))
+
+    def write(self, text):
+        """Gets text input from the user.
+        
+        Args:
+            self (Console): an instance of Console.
+            text(string): The output to be displayed.
+        """
+        print(text)
