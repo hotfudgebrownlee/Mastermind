665e5c29eb8b6a2984c1e5333919072275050f28
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-02-13 08:52:45 -0700

    move class

--- mastermind/game/move.py
+++ mastermind/game/move.py
@@ -2,12 +2,16 @@ class Move:
     """
     Stereotype: 
         Information Holder
-
     """
-    def __init__(self):
+    def __init__(self, guess):
         """The class constructor.
-        
         Args:
             self (Move): an instance of Move.
         """
-        pass
+        self._guess = guess 
+    def get_guess(self):
+        """Returns the move that is either correct or not.
+        Args:
+            self(Move): an instance of move.
+        """  
+        return self._guess
