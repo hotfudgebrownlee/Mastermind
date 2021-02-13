4bc1df80d56f302e158de08193ef96ba0f9e4cc6
dtrinhh <dustintrinhh@gmail.com>
2021-02-12 22:40:31 -0700

    added functions

--- mastermind/game/player.py
+++ mastermind/game/player.py
@@ -3,10 +3,42 @@ class Player:
     Stereotype: 
         Information Holder
     """
-    def __init__(self):
+    def __init__(self, name):
         """The class constructor.
         
         Args:
             self (Player): an instance of Player.
         """
-        pass
+        self._name = name
+        self._move = None
+
+    def get_name(self):
+        """
+        Returns the player's name.
+
+        Args:
+            self (Player): an instance of Player.
+        """
+        return self._name
+    
+    def get_move(self):
+        """
+        Returns the player's last move (an instance of Move). If the player 
+        hasn't moved yet this method returns None.
+
+        Args:
+            self (Player): an instance of Player.
+        """
+        return self._move
+
+    def set_move(self, move):
+        """
+        Sets the player's last move to the given instance of Move.
+
+        Args:
+            self (Player): an instance of Player.
+            move (Move): an instance of Move
+        """
+        self._move = move
+
+    
